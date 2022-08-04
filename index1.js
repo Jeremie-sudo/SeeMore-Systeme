84cd34b9670c6c2c434c39c5c91ce76aeec4f67e
Olakounle <jeremieadjeolakounle@gmail.com>
2022-08-04 14:32:53 +0100

    Petit Projet

---
+++ index1.js
@@ -0,0 +1,11 @@
+const show = document.querySelector('h3');
+
+const infos = document.querySelector('.infos');
+
+
+show.addEventListener('click',()=>{
+
+     infos.classList.toggle("showOrhide");
+
+
+})
